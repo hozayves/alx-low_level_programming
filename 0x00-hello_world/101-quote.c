3d5c6b24fb39e59b1037abc00486c49d4eea2c5f
hozayves <muhozayves2014@gmail.com>
2023-02-16 12:22:13 -0800

    8th challenge

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, sentence, 59);
 	return (1);
 }
