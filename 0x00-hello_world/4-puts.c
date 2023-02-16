072adfa594421264835d0befbc585643d35168de
hozayves <muhozayves2014@gmail.com>
2023-02-16 08:57:01 -0800

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-
 /*
- * main - Entry point
- *
- * Description: A C programm that prints with put function
- *
- * Return: Alway 0 (Success)
- *
+ * name : Entry point
+ * Description: A program that print simple message
+ * Return : 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
