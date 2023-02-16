ec8fae3de722207b51e1b260980908271eecc596
hozayves <muhozayves2014@gmail.com>
2023-02-16 09:33:19 -0800

    4th challenges

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Description: A program for display short message
+ *
+ * Return: Always 0 (Success)
+ * */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
