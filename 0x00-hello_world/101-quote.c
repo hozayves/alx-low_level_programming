d7e149ff666c59a14d48dd939b94e96e3492ebb8
hozayves <muhozayves2014@gmail.com>
2023-02-16 12:31:58 -0800

    8th challenges

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 /**
- * Main: Entry point
+ * Main- Entry point
  *
  * Description: A simple program that display without puts or prints
- * WE're goinf to include unistd library
+ * we're goinf to include unistd library
  *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, sentence, 59);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, msg, 59);
 	return (1);
 }
