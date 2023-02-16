5b9a590f7941357fafec28ca68615dbc63c70195
hozayves <muhozayves2014@gmail.com>
2023-02-16 12:35:04 -0800

    8th challenges

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
+
 /**
- * Main- Entry point
+ * main - Entry point
  *
  * Description: A simple program that display without puts or prints
  * we're goinf to include unistd library
  *
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
