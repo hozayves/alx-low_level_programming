1dddce7a61f320e88c7b6ee9f302112e48de9a8c
hozayves <muhozayves2014@gmail.com>
2023-02-17 13:35:18 -0800

    8th challenge

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* base16 prototype */
+int base16(void);
+/**
+ * main - Entry point
+ *
+ * Description: A program that display a digit of base 16
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	base16();
+	return (0);
+}
+/**
+ * base16 - function definition
+ *
+ * Description: A function that receive nothing
+ *
+ * Return: nothing
+ */
+int base16(void)
+{
+	int i;
+	char z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	for (z = 'a'; z <= 'f'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+}
