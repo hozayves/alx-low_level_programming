9f190af6228045aa64843c95c4ac582477518647
hozayves <muhozayves2014@gmail.com>
2023-02-17 10:44:00 -0800

    4th challenge C Level-1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A programm that display alphabet except q and e
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
