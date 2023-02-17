4eb52df38e25e919395a77567d50442ff83f20da
hozayves <muhozayves2014@gmail.com>
2023-02-17 10:20:53 -0800

    3rd challenge C Level-1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+/* Function prototype for alphabet */
+void Alphabet(void);
+/**
+ * main - Entry point
+ *
+ * Description: A program that display alphabetical
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	alphabet();
+	putchar('\n');
+	return (0);
+}
+/**
+ * Alphabet - no entry value
+ * @void: Not entry value
+ *
+ * Function: for loops and display alphabetic from a to z
+ *
+ * Return: nothing
+ */
+void Alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		putchar(toupper(ch));
+	}
+}
