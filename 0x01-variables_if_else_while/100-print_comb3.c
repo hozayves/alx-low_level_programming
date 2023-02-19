d2e07b9553b039cccc781c121551e8fae68cb8b4
hozayves <muhozayves2014@gmail.com>
2023-02-19 12:46:59 -0800

    10th challenge cod refactoring

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,23 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1 = 0;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	while (digit1 < 9)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+		int digit2 = digit1 + 1;
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			putchar(',');
+		while (digit2 < 10)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+
+			if (digit1 != 8 || digit2 != 9)
+				putchar(',');
 			putchar(' ');
+			digit2++;
 		}
+		digit1++;
 	}
 
 	putchar('\n');
