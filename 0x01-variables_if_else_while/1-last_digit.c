b7538abe024a75d8b9306fab6abcfca26092f5b1
hozayves <muhozayves2014@gmail.com>
2023-02-19 09:56:53 -0800

    1st challenge refactor codes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* Function call */
-int lastNum(int n);
 /**
  * main - Entry point
  *
- * Description: A program find last digit and make comparison
+ * Description: A program that display last digit of n number
+ * and check if positive, negative or zero
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, lastNum(n));
-	if (lastNum(n) > 5)
+	last_digit = n % 10;
+	printf("Last digit of %d is %d ", n, last_digit);
+	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	else if (lastNum(n) < 6 && lastNum(n) != 0)
+	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
@@ -31,15 +30,4 @@ int main(void)
 		printf("and is 0\n");
 	}
 	return (0);
-}
-
-/**
- * lastNum - Find a last digit of a number
- * @n: First operand
- *
- * Return: The last last digit
- */
-int lastNum(int n)
-{
-	return (n % 10);
 }
