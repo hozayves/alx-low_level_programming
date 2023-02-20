1624f0c75f8aad0009aeeafd87e597278ffe2945
hozayves <muhozayves2014@gmail.com>
2023-02-20 10:26:33 -0800

    102 print comb code refactoring

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - Entry point
+ * Description: A program thatprints all possible
+ * combinations of two two-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
