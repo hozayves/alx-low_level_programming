6f908a0d8747e0886968f753e517611ad077bed6
hozayves <muhozayves2014@gmail.com>
2023-02-21 09:07:29 -0800

    2nd challenge :bookmark: :zap:

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function definition
+ *
+ * Description: A program that print alphabet 10x
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+
+		i++;
+	}
+}
