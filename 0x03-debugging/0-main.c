89a7a26eb6c33af378cfbefcc77ff65f775eae03
hozayves <muhozayves2014@gmail.com>
2023-02-22 07:29:09 -0800

    Debugging :package: :fire::fire: :art: :apple:

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: A program that print if integer is positive or negative
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
