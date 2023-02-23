47af34c1e0148f8d171aeac22f804144efab0ef2
hozayves <muhozayves2014@gmail.com>
2023-02-23 04:19:32 -0800

    2nd challenges :boom::fire::fire:

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function definition
+ *
+ * Description: A function that multiple two integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Retrn: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
