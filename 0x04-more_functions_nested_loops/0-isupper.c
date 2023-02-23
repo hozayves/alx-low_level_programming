2ee02b1e06c9d6a2c9f37298d0accae44e8861ab
hozayves <muhozayves2014@gmail.com>
2023-02-23 03:47:18 -0800

    0th challenge | 0-isupper :fire::boom::package:

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - Function defition
+ * 
+ * Description: A function that checks for uppercase character
+ * @c: parameter
+ *
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
