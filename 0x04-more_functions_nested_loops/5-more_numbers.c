674ffffabfae2caa3a66c7fd75af75f2dd35aa52
hozayves <muhozayves2014@gmail.com>
2023-02-23 06:47:06 -0800

    5th challenges more numbers :fire::fire::art:

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - Function definition
+ *
+ * Description: A function that prints 10 times then numbers, from 0 to 14
+ *
+ * Return: nothings
+ */
+void more_numbers(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar(j + 48);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
