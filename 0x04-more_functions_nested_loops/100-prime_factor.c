f38fa8e59e349e62e8727648134873dbb918ee7f
hozayves <muhozayves2014@gmail.com>
2023-02-24 12:18:32 -0800

    11th challenge prime-factor :rocket::fire::fire:

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that finds and prints the largest prime factor
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c = 2, n = 1231952;
+
+	while (n > 1)
+	{
+		if (n % c == 0)
+		{
+			printf("%d ", c);
+			n /= c;
+		}
+		else
+		{
+			c++;
+		}
+	}
+	return (0);
+}
