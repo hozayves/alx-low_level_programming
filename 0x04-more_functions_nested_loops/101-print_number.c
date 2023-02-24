1115eaa549edb6b4f18425eb95c677f2ee99c0df
hozayves <muhozayves2014@gmail.com>
2023-02-24 14:41:25 -0800

    Advanced print_number :package::art:

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+void long_int(int);
+
+/**
+ * print_number - function definition
+ *
+ * Description: a function that prints an integer
+ * @n: parameter
+ *
+ * Return: result
+ */
+void print_number(int n)
+{
+	long_int(n);
+}
+void long_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		long_int(n / 10);
+	_putchar(n % 10 + 48);
+}
