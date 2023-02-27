f5854c5895f65916bf6c0b072d477664d4156de6
hozayves <muhozayves2014@gmail.com>
2023-02-27 11:17:39 -0800

    3rd challenges:rocket::rocket::fire:

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function definition
+ *
+ * Description: A function that prints a string, followed by new line
+ * @str: parameter
+ *
+ * Return: result
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
