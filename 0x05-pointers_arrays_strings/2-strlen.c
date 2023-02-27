bd4eafd6337d264e356d7185884fd566f7483c0f
hozayves <muhozayves2014@gmail.com>
2023-02-27 11:07:36 -0800

    2nd challenges string with pointer:art::fire::fire:

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function defition
+ *
+ * Description: A function that returs the length of a string
+ * @s: parameter
+ *
+ * Return: result
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
