f4f457e4c35fb020fdbadb60ede5c0343d82c919
hozayves <muhozayves2014@gmail.com>
2023-02-27 03:28:51 -0800

    0th challenge :rocket::fire:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function prototype
+ *
+ * Description: A function that update value throught its address
+ * @n: parameter
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
