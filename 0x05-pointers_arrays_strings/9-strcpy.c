f42aaf89b5a2f151915d76fdbe3dd56cca9df555
hozayves <muhozayves2014@gmail.com>
2023-02-28 02:14:02 -0800

    9th challenge #strcpy :rocket::rocket::fire::fire:

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - function definition
+ *
+ * Description: A function that copies the string pointed to by src
+ * @dest: destination parameter
+ * @src: source	parameter
+ *
+ * Return: result
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
