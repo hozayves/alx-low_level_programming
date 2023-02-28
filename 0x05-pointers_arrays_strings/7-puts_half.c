ff0d03233cfa51afc90a32956443d74cc36ec283
hozayves <muhozayves2014@gmail.com>
2023-02-28 03:35:45 -0800

    7th challenge :rocket::art::art::fire:

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - function definition
+ *
+ * Description: A function that prints half of a string, end with \n
+ * @str: string parameter
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, size = strlen(str);
+
+	if (size % 2 != 0)
+	{
+		for (i = (size - 1) / 2; i < size; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = size / 2; i < size; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+_putchar('\n');
+}
