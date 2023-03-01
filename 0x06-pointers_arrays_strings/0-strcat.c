b9a56f956c89311803e8b8cc2536ccb64d3a11ca
hozayves <muhozayves2014@gmail.com>
2023-03-01 10:35:00 -0800

    0th challenge string concatenation:rocket:

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - function definition
+ *
+ * Description: A function that concatenates two strings
+ * @dest: destination parameter
+ * @src: source parameter
+ *
+ * Return: result
+ */
+char *_strcat(char *dest char *src)
+{
+	
+}
