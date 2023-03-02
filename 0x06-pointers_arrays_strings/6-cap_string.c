875e78301d364d7691ce043cea55f0a9e03cf72b
hozayves <muhozayves2014@gmail.com>
2023-03-02 13:09:57 -0800

    6th challenges :rocket::rocket::fire:

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - function definition
+ *
+ * Description: A function that capitalize all worlds of a string
+ * @string: parameter
+ *
+ * Return: result
+ */
+char *cat_string(char *string)
+{
+	int i;
+	int capitalize_next = 1;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if(isspace(string[i]))
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next)
+		{
+			string[i] = toupper(string[i]);
+			capitalize_next = 0;
+		}
+	}
+}
