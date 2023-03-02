a8beabbdf2acc4a6cd14bd2fff9d2629065cca71
hozayves <muhozayves2014@gmail.com>
2023-03-02 06:54:01 -0800

    3rd challenges:rocket:

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcmp - function definition
+ *
+ * Description: A function that compares two strings
+ * @s1: string1 parameter
+ * @s2: string2 parameter
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[2];
+		}
+		i++;
+	}
+	return (r);
+}
