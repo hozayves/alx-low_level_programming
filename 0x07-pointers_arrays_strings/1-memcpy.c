8a0ba37cfa3967cac0b6898a6fad90b0db9747df
hozayves <muhozayves2014@gmail.com>
2023-03-06 10:59:36 -0800

    betty fix error

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,12 +6,12 @@
  * @dest: destination parameter
  * @src: source parameter
  * @n: number of bytes to copy
- * 
+ *
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned i, j = 0;
+	unsigned int i, j = 0;
 
 	for (i = 0; i < n; i++)
 	{
