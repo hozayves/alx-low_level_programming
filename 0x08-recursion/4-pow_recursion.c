c4b7c20dd6493736d75874fe6ff687d75bfd3f6b
hozayves <muhozayves2014@gmail.com>
2023-03-08 10:40:29 -0800

    4th challenge:zap::fire::package::rocket:

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - function definition
+ *
+ * Description: A function that returs th value of x raised to the power of y
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y > 1)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
