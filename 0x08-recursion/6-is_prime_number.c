c2ec1912c577f9f68368e6a9277b793a4dce12b9
hozayves <muhozayves2014@gmail.com>
2023-03-08 14:12:27 -0800

    6th challenges :zap::rocket::rocket:

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * find _prime - function
+ *
+ * Description: A function that find a prime number
+ * @n: number parameter
+ * @i: iterator parameter
+ *
+ * Return: 0 or 1
+ */
+int find_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (find_prime(n, i - 1));
+}
+/**
+ * is_prime_number - function definition
+ *
+ * Description: A function that check if it's prime or not
+ * @n: number paramter
+ *
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (n, n - 1);
+}
