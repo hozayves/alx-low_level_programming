316b77b9351adcbcecef0ec462dde83c866a3585
hozayves <muhozayves2014@gmail.com>
2023-03-08 05:22:28 -0800

    0th challenges :fire::package::rocket:

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function definition
+ *
+ * Description: A function prints a string, followed by new line
+ * @str: parameter
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *str)
+{
+
+	printf("%s\n", str);
+}
