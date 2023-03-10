f4ef81f3c1862252ccd9aea43011c177cb52596e
hozayves <muhozayves2014@gmail.com>
2023-03-10 14:19:16 -0800

    Task 0 :package::rocket::rocket::zap:

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: A function that prints its name
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
