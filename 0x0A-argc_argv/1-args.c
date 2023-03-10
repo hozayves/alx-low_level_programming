4d047726da8d03e5d036faf5a1a392b7f6590a7f
hozayves <muhozayves2014@gmail.com>
2023-03-10 15:12:01 -0800

    1st challenge :rocket::rocket::zap::fire:

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - function main
+ *
+ * Description: A program that prints the number of arguments passed into it
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ *
+ * Return: 0 (success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
