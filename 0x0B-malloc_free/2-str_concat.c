11add4ef7dcc04aa173a3e0e61ce0e28567f2404
hozayves <muhozayves2014@gmail.com>
2023-03-14 14:39:12 -0700

    2nd challenge:fire:

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * str_concat - function definition
+ *
+ * Description: A function that concatenates two strings
+ * @s1: string1 parameter
+ * @s2: string2 parameter
+ *
+ * Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0;
+	char *concat;
+
+}
