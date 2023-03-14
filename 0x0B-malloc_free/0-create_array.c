99edc11b2cb207b3e3919698e745ecd90010547c
hozayves <muhozayves2014@gmail.com>
2023-03-14 06:33:35 -0700

    change type:bug:

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int *arr = (char *) malloc(size * sizeof(char));
+	char *arr = (char *) malloc(size * sizeof(char));
 
 	if (size == 0 || arr == 0)
 		return (NULL);
