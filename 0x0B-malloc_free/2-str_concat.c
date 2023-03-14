a340e1d8b11fa3cb270143d3066b850c7fbf6607
hozayves <muhozayves2014@gmail.com>
2023-03-14 16:13:29 -0700

    2nd challenge:rocket::rocket:

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0, size2 = 0, i, j;
 	char *concat;
 	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size1 = strlen(s1);
 	size2 = strlen(s2);
 	concat = (char *)malloc((sizeof(char) * (size1 + size2)) + 1);
-
+	if (concat == NULL)
+		return (NULL);
 
 	for (i = 0; i < size1; i++)
 	{
@@ -26,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 	for (j = i; j < size2; j++)
 	{
 		concat[i] = s2[i];
+		i++;
 	}
 	return (concat);
 }
