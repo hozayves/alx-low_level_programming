8006d728a2bcbbf53b4d4080b9ca7feb151d435b
hozayves <muhozayves2014@gmail.com>
2023-03-14 14:49:07 -0700

    :rocket:

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1 = 0, size2 = 0;
+	int size1 = 0, size2 = 0, i;
 	char *concat;
+	
+	size1 = strlen(s1);
+	size2 = strlen(s2);
+	concat = (char *)malloc((sizeof(char) * (size1 + size2)) + 1);
 
+
+	for (i = 0; i < size1; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = i; j < size2; j++)
+	{
+		concat[i] = s2[i];
+	}
+	return (concat);
 }
