3b3e14c941c2dd012e91dc64446880b3b1837dfb
hozayves <muhozayves2014@gmail.com>
2023-03-14 16:19:13 -0700

    :bug::bug::rocket::fire:

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1 = 0, size2 = 0, i, j;
+	int size1, size2, i, j;
 	char *concat;
 	
 	if (s1 == NULL)
@@ -33,5 +33,6 @@ char *str_concat(char *s1, char *s2)
 		concat[i] = s2[i];
 		i++;
 	}
+	concat[i] = '\0';
 	return (concat);
 }
