80681051dc70b4f911f9a7487c9d3008327b7c66
hozayves <muhozayves2014@gmail.com>
2023-03-14 17:21:13 -0700

    3rd challenge:rocket:

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * alloc_grid - function definition
+ *
+ * Description: A function that returns a pointer to a 2 dime
+ * array of integers
+ * @width: parameter
+ * @height: parameter
+ *
+ * Return: grid 
+ */
+int **alloc_grid(int width, int height)
+{
+	int **gridout;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
+}
