b82bd23a6e7ecb2fcfad4c2b3daebfbab6b44e6b
hozayves <muhozayves2014@gmail.com>
2023-03-16 07:13:26 -0700

    0th challenge :art::package::zap::zap:

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - function definition
+ *
+ * Description: A function that allocates memory using malloc
+ * @b: parameter
+ *
+ * Return: pointer of the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (malloc(b) == NULL)
+		return ("98");
+	return (malloc(b));
+}
