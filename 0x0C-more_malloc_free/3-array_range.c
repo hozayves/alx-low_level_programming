32c5d635dc0ccdf43b71854fc91a4bfab60f2414
hozayves <muhozayves2014@gmail.com>
2023-03-16 16:20:41 -0700

    3rd challenges :rocket::zap::zap::fire:

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * array_range - function definition
+ *
+ * Description: A function that creates an array of integer
+ * @min: minumum parameter
+ * @max: maximum parameter
+ *
+ * Return: pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int i, *ptr_reserve;
+
+	if (min > max)
+		return (NULL);
+	ptr_reserve = (int *)malloc(sizeof(int) * (max - min + 1));
+	if (ptr_reserve == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		ptr_reserve[i] = i;
+	}
+	return (ptr_reserve);
+}
