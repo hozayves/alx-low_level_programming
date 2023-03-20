e37439ba64471fa085e55741ab808da31abdda6f
hozayves <muhozayves2014@gmail.com>
2023-03-20 15:17:49 -0700

    Betty style code :bug:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG
 #define _DOG
-
+/**
+ * struct dog - define struct for dog
+ * @name: dog's name parameter
+ * @age: dog's age parameter
+ * @owner: do's owner parameter
+ */
 struct dog
 {
 	char *name;
