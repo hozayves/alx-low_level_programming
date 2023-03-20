11c4872ec6cf629a76184b5e7447fde417e0f824
hozayves <muhozayves2014@gmail.com>
2023-03-20 08:47:51 -0700

    2nd challenge :rocket::zap::zap::fire:

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: A function that prints the name of the file
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
