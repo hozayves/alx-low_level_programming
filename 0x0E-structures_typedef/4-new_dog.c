4b83f0b8cd1f6ef77e232b170680acdce11519cc
hozayves <muhozayves2014@gmail.com>
2023-03-21 16:06:24 -0700

    check when malloc does not allocate space:bug:

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *_dog;
 
 	_dog = malloc(sizeof(dog_t));
-
+	if (_dog == NULL)
+		return (NULL);
 	_dog->name = name;
 	_dog->age = age;
 	_dog->owner = owner;
