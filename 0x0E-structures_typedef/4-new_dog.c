38ddc3f453f507a7bea10886473eebb401e86ea8
hozayves <muhozayves2014@gmail.com>
2023-03-21 16:00:15 -0700

    4th challenges :rocket::zap::zap:

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - A function that creates a new dog
+ * @name: dog's name parameter
+ * @age: dog's age parameter
+ * @owner: dog's owner parameter
+ *
+ * Return: a pointer of new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *_dog;
+
+	_dog = malloc(sizeof(dog_t));
+
+	_dog->name = name;
+	_dog->age = age;
+	_dog->owner = owner;
+
+	return (_dog);
+}
