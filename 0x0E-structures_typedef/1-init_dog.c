e360505f78dcae2b98d21cafbd35e87479b6cba6
hozayves <muhozayves2014@gmail.com>
2023-03-21 12:28:54 -0700

    1st challenge :rocket::rocket::zap:

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - function definition
+ * Description: A function that initialize a variable of type struct dog
+ * @d: parameter
+ * @name: name parameter
+ * @age: age parameter
+ * @owner: owner paremeter
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
