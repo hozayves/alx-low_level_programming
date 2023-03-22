e3c94da0fe56c308feb043d2d9ae67195973add3
hozayves <muhozayves2014@gmail.com>
2023-03-22 13:38:30 -0700

    include stddef library Add array_iterator function prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef POINTERS
 #define POINTERS
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
