9de57b0bfd38d3f9301f4820dd532a573785d8b2
hozayves <muhozayves2014@gmail.com>
2023-03-22 16:14:07 -0700

    3rd challenges :rocket:

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include  <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ....
+ * @argc: ....
+ * @argv: .....
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
