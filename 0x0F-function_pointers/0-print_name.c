627b18f1ca3f9b4c886183d32b14ff8decfb7ed8
hozayves <muhozayves2014@gmail.com>
2023-03-22 09:32:52 -0700

    0th challenge :rocket::zap::zap:

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - function definition
  *
@@ -10,5 +11,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
