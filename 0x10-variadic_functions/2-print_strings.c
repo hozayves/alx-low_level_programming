d8d156e2f62a51619093d7cde7435a0b743a62dc
hozayves <muhozayves2014@gmail.com>
2023-03-23 08:30:39 -0700

    2nd challenges :rocket::rocket::zap:

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function definition
+ *
+ * Description: A function that prints a strings, followed by new line
+ * @separator: string separator parameter
+ * @n: number of string to print parameter
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list pString;
+	unsigned int i;
+
+	va_start(pString, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(pString, char *));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(pString);
+}
