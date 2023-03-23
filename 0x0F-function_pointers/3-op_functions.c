da52b6b0ff5d11cf424fc6a0be6515b42f7fba7b
hozayves <muhozayves2014@gmail.com>
2023-03-22 17:17:59 -0700

    Fix logic error

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,61 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * op_add - function for return sum
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @a: 1st parameter
- * @b: 2nd parameter
- *
- * Return: su
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - ....
- * @a: ....
- * @b: ....
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: ....
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - ....
- * @a: ....
- * @b: .....
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: ....
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - ....
- * @a: ....
- * @b: .....
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: ....
+ * Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
+
 /**
- * op_mod - ......
- * @a: .......
- * @b: .......
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: ....
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
-
 	return (a % b);
 }
