d1d998235117bace8d0b047291486d6d6b01f5f3
hozayves <muhozayves2014@gmail.com>
2023-04-04 14:51:36 -0700

    5th challenge

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - A function that frees a listint_t list
+ * @head: pointer to first node
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *ptr;
+
+	while (current != NULL)
+	{
+		ptr = current->next;
+		free(current);
+		current = ptr;
+	}
+	head = NULL;
+}
